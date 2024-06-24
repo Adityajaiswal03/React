5c2924b5f757339d8f8db8a487cd8ac4f21380d7
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-06-24 10:17:11 +0530

    Redux-II

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore} from "redux";
 function cartReducer(state={items:{}},action){
     switch(action.type){
         case "ADD_TO_CART":{
-            
+
         }
         case "REMOVE_FROM_CART":{
 
