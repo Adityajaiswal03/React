5c6482a2e102873d8de0d13cd47a8b4b1baf3564
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-06-24 11:04:02 +0530

    Redux-Cart-Function

--- src/Components/ReduxAddToCart/ReduxAddToCart.js
+++ src/Components/ReduxAddToCart/ReduxAddToCart.js
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import CartContext from "../../Context/CartContext";
+import { useDispatch } from "react-redux";
 
 function AddToCart({ product }) {
     // useSelector
+    let dispatch=useDispatch();
     function increase() {
         //dispatcher
+        dispatch({type:"ADD_TO_CART",payload:product});
     }
     function decrease() {
         //dispatcher
+        dispatch({type:"REMOVE_FROM_CART",payload:product});
     }
+    let cart=useSelector(()=>{
+        return state.items;
+    })
     const quanitity = cart[product.id] ? cart[product.id].quanitity : 0;
     if (quanitity === 0) {
         return (
